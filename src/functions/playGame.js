bc470975b692a2eeb4e07204d90d29318f84d7ad
NLeebs <lieberman.nathan@gmail.com>
2023-06-22 16:03:05 -0400

    Adding functions and play game file to app

---
+++ src/functions/playGame.js
@@ -0,0 +1,25 @@
+// Determine Possessions in game
+
+// Strength of Schedule Adjustment
+// Adjusts weight of stats in a team's direction
+
+// Chance of turnover
+// Offensive rate vs Defensiver rate
+
+// Chance of foul
+// offensive cause foul vs defensive commit foul
+// after 7 shoot free throws
+
+// Was 2 or 3
+// Offense percentage of shots
+
+// Chance of Scoring
+// Offensive type Shooting Percentage vs defense percentage
+
+// Chance of rebound
+// Offense rebound vs defensive rebound
+// Set start of next possession
+
+// loop through all possessions
+
+// returns object of winner, loser, and both scores
