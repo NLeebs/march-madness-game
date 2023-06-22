46ab00f965c6f64c6ff3ad32eb9868fde13ea9d4
NLeebs <lieberman.nathan@gmail.com>
2023-06-22 16:03:42 -0400

    Added bracket componenets to the app

---
+++ src/components/Bracket/BracketRound.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function BracketRound({ teams }) {
+  return (
+    <div className="flex flex-col space-y-2">
+      {teams.map((team, index) => (
+        <div
+          key={index}
+          className={`w-24 h-10 bg-white border-2 rounded-md flex justify-center items-center ${
+            index % 2 === 0 ? "border-blue-600" : "border-red-600"
+          }`}
+        >
+          <p className="text-sm">{team}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default BracketRound;
