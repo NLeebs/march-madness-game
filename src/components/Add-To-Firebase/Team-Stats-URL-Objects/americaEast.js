88a170ceef74609306f6d1e045ae0f96dd486504
NLeebs <lieberman.nathan@gmail.com>
2023-07-23 16:00:28 -0400

    Updated objects with new site URLS Fixed UCF bugs

--- src/components/Add-To-Firebase/Team-Stats-URL-Objects/americaEast.js
+++ src/components/Add-To-Firebase/Team-Stats-URL-Objects/americaEast.js
@@ -1,46 +1,46 @@
 const americaEastStatURLs = {
     "Albany": {
-        "team-colors-URL": "https://usteamcolors.com/university-at-albany-colors/",
+        "team-colors-URL": "https://teamcolorcodes.com/albany-great-danes-colors/",
         "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/albany-great-danes/stats",
         "team-logo-URL": "team-logos\\americanEast\\AlbanyLogo.png",
     },
     "Binghamton": {
-        "team-colors-URL": "https://usteamcolors.com/binghamton-university-colors/",
+        "team-colors-URL": "https://teamcolorcodes.com/binghamton-bearcats-colors/",
         "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/binghamton-bearcats/stats",
         "team-logo-URL": "team-logos\\americanEast\\BinghamtonLogo.png",
     },
     "Bryant": {
-        "team-colors-URL": "https://usteamcolors.com/bryant-university-colors/",
+        "team-colors-URL": "https://teamcolorcodes.com/bryant-bulldogs-colors/",
         "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/bryant-bulldogs/stats",
         "team-logo-URL": "team-logos\\americanEast\\BryantLogo.png",
     },
     "Maine": {
-        "team-colors-URL": "https://usteamcolors.com/university-of-maine-colors/",
+        "team-colors-URL": "https://teamcolorcodes.com/maine-black-bears-color-codes/",
         "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/maine-black-bears/stats",
         "team-logo-URL": "team-logos\\americanEast\\MaineLogo.png",
     },
     "NJIT": {
-        "team-colors-URL": "https://usteamcolors.com/new-jersey-institute-of-technology-colors/",
+        "team-colors-URL": "https://teamcolorcodes.com/njit-highlanders-color-codes/",
         "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/njit-highlanders/stats",
         "team-logo-URL": "team-logos\\americanEast\\NJITLogo.png",
     },
     "New Hampshire": {
-        "team-colors-URL": "https://usteamcolors.com/university-of-new-hampshire-colors/",
+        "team-colors-URL": "https://teamcolorcodes.com/new-hampshire-wildcats-color-codes/",
         "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/new-hampshire-wildcats/stats",
         "team-logo-URL": "team-logos\\americanEast\\NewHampshireLogo.png",
     },
     "UMBC": {
-        "team-colors-URL": "https://usteamcolors.com/umbc-colors/",
+        "team-colors-URL": "https://teamcolorcodes.com/umbc-retrievers-colors/",
         "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/umbc-retrievers/stats",
         "team-logo-URL": "team-logos\\americanEast\\UMBCLogo.png",
     },
     "Massachusetts Lowell River": {
-        "team-colors-URL": "https://usteamcolors.com/umass-lowell-colors/",
+        "team-colors-URL": "https://teamcolorcodes.com/umass-lowell-river-hawks/",
         "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/massachusetts-lowell-river-hawks/stats",
         "team-logo-URL": "team-logos\\americanEast\\MassachusettsLowellRiverLogo.png",
     },
     "Vermont": {
-        "team-colors-URL": "https://usteamcolors.com/university-of-vermont-colors/",
+        "team-colors-URL": "https://teamcolorcodes.com/vermont-catamounts-color-codes/",
         "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/vermont-catamounts/stats",
         "team-logo-URL": "team-logos\\americanEast\\VermontLogo.png",
     },
