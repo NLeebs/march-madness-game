e46379918ee21580357b8446f2c74768a62dca11
NLeebs <lieberman.nathan@gmail.com>
2023-07-26 17:05:15 -0400

    Changed logo url structure

--- src/components/Add-To-Firebase/Team-Stats-URL-Objects/americaEast.js
+++ src/components/Add-To-Firebase/Team-Stats-URL-Objects/americaEast.js
@@ -2,47 +2,52 @@ const americaEastStatURLs = {
     "Albany": {
         "team-colors-URL": "https://teamcolorcodes.com/albany-great-danes-colors/",
         "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/albany-great-danes/stats",
-        "team-logo-URL": "team-logos\\americanEast\\AlbanyLogo.png",
+        "team-logo-URL": "@\/public\/team-logos\/americanEast\/AlbanyLogo.png",
     },
     "Binghamton": {
         "team-colors-URL": "https://teamcolorcodes.com/binghamton-bearcats-colors/",
         "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/binghamton-bearcats/stats",
-        "team-logo-URL": "team-logos\\americanEast\\BinghamtonLogo.png",
+        "team-logo-URL": "@\/public\/team-logos\/americanEast\/BinghamtonLogo.png",
+        "mascot-name": "Bearcats",
     },
     "Bryant": {
         "team-colors-URL": "https://teamcolorcodes.com/bryant-bulldogs-colors/",
         "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/bryant-bulldogs/stats",
-        "team-logo-URL": "team-logos\\americanEast\\BryantLogo.png",
+        "team-logo-URL": "@\/public\/team-logos\/americanEast\/BryantLogo.png",
+        "mascot-name": "Bulldogs",
     },
     "Maine": {
         "team-colors-URL": "https://teamcolorcodes.com/maine-black-bears-color-codes/",
         "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/maine-black-bears/stats",
-        "team-logo-URL": "team-logos\\americanEast\\MaineLogo.png",
+        "team-logo-URL": "@\/public\/team-logos\/americanEast\/MaineLogo.png",
     },
     "NJIT": {
         "team-colors-URL": "https://teamcolorcodes.com/njit-highlanders-color-codes/",
         "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/njit-highlanders/stats",
-        "team-logo-URL": "team-logos\\americanEast\\NJITLogo.png",
+        "team-logo-URL": "@\/public\/team-logos\/americanEast\/NJITLogo.png",
     },
     "New Hampshire": {
         "team-colors-URL": "https://teamcolorcodes.com/new-hampshire-wildcats-color-codes/",
         "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/new-hampshire-wildcats/stats",
-        "team-logo-URL": "team-logos\\americanEast\\NewHampshireLogo.png",
+        "team-logo-URL": "@\/public\/team-logos\/americanEast\/NewHampshireLogo.png",
+        "list-name": "N Hampshire",
     },
     "UMBC": {
         "team-colors-URL": "https://teamcolorcodes.com/umbc-retrievers-colors/",
         "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/umbc-retrievers/stats",
-        "team-logo-URL": "team-logos\\americanEast\\UMBCLogo.png",
+        "team-logo-URL": "@\/public\/team-logos\/americanEast\/UMBCLogo.png",
+        "list-name": "Maryland BC",
     },
     "Massachusetts Lowell River": {
         "team-colors-URL": "https://teamcolorcodes.com/umass-lowell-river-hawks/",
         "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/massachusetts-lowell-river-hawks/stats",
-        "team-logo-URL": "team-logos\\americanEast\\MassachusettsLowellRiverLogo.png",
+        "team-logo-URL": "@\/public\/team-logos\/americanEast\/MassachusettsLowellRiverLogo.png",
+        "list-name": "Mass Lowell",
     },
     "Vermont": {
         "team-colors-URL": "https://teamcolorcodes.com/vermont-catamounts-color-codes/",
         "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/vermont-catamounts/stats",
-        "team-logo-URL": "team-logos\\americanEast\\VermontLogo.png",
+        "team-logo-URL": "@\/public\/team-logos\/americanEast\/VermontLogo.png",
     },
 }
 
--- src/components/Add-To-Firebase/Team-Stats-URL-Objects/bigEast.js
+++ src/components/Add-To-Firebase/Team-Stats-URL-Objects/bigEast.js
@@ -2,61 +2,61 @@ const bigEastStatURLs = {
     "Butler": {
         "team-colors-URL": "https://teamcolorcodes.com/butler-bulldogs-color-codes/",
         "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/butler-bulldogs/stats",
-        "team-logo-URL": "team-logos\\bigEast\\ButlerLogo.png",
+        "team-logo-URL": "@\/public\/team-logos\/bigEast\/ButlerLogo.png",
     },
     "Creighton": {
         "team-colors-URL": "https://teamcolorcodes.com/creighton-bluejays-color-codes/",
         "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/creighton-bluejays/stats",
-        "team-logo-URL": "team-logos\\bigEast\\CreightonLogo.png",
+        "team-logo-URL": "@\/public\/team-logos\/bigEast\/CreightonLogo.png",
     },
     "DePaul": {
         "team-colors-URL": "https://teamcolorcodes.com/depaul-blue-demons-color-codes/",
         "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/depaul-blue-demons/stats",
-        "team-logo-URL": "team-logos\\bigEast\\DePaulLogo.png",
+        "team-logo-URL": "@\/public\/team-logos\/bigEast\/DePaulLogo.png",
     },
     "Georgetown": {
         "team-colors-URL": "https://teamcolorcodes.com/georgetown-hoyas-colors/",
         "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/georgetown-hoyas/stats",
-        "team-logo-URL": "team-logos\\bigEast\\GeorgetownLogo.png",
+        "team-logo-URL": "@\/public\/team-logos\/bigEast\/GeorgetownLogo.png",
     },
     "Marquette": {
         "team-colors-URL": "https://teamcolorcodes.com/marquette-colors/",
         "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/marquette-golden-eagles/stats",
-        "team-logo-URL": "team-logos\\bigEast\\MarquetteLogo.png",
+        "team-logo-URL": "@\/public\/team-logos\/bigEast\/MarquetteLogo.png",
     },
     "Providence": {
         "team-colors-URL": "https://teamcolorcodes.com/providence-friars-color-codes/",
         "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/providence-friars/stats",
-        "team-logo-URL": "team-logos\\bigEast\\ProvidenceLogo.png",
+        "team-logo-URL": "@\/public\/team-logos\/bigEast\/ProvidenceLogo.png",
     },
     "Seton Hall": {
         "team-colors-URL": "https://teamcolorcodes.com/seton-hall-pirates-colors/",
         "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/seton-hall-pirates/stats",
-        "team-logo-URL": "team-logos\\bigEast\\SetonHallLogo.png",
+        "team-logo-URL": "@\/public\/team-logos\/bigEast\/SetonHallLogo.png",
     },
     "St. John's": {
         "team-colors-URL": "https://teamcolorcodes.com/st-johns-red-storm-color-codes/",
         "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/st-johns-red-storm/stats",
-        "team-logo-URL": "team-logos\\bigEast\\StJohnsLogo.png",
+        "team-logo-URL": "@\/public\/team-logos\/bigEast\/StJohnsLogo.png",
         "list-name": "St Johns",
         "mascot-name": "Red Storm",
     },
     "UConn": {
         "team-colors-URL": "https://teamcolorcodes.com/connecticut-huskies-colors/",
-        "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/connecticut-huskies",
-        "team-logo-URL": "team-logos\\bigEast\\UConnLogo.png",
+        "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/connecticut-huskies/stats",
+        "team-logo-URL": "@\/public\/team-logos\/bigEast\/UConnLogo.png",
         "list-name": "Connecticut",
         "mascot-name": "Huskies",
     },
     "Villanova": {
         "team-colors-URL": "https://teamcolorcodes.com/villanova-wildcats-colors/",
         "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/villanova-wildcats/stats",
-        "team-logo-URL": "team-logos\\bigEast\\VillanovaLogo.png",
+        "team-logo-URL": "@\/public\/team-logos\/bigEast\/VillanovaLogo.png",
     },
     "Xavier": {
         "team-colors-URL": "https://teamcolorcodes.com/xavier-university-musketeers-colors/",
         "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/xavier-musketeers/stats",
-        "team-logo-URL": "team-logos\\bigEast\\XavierLogo.png",
+        "team-logo-URL": "@\/public\/team-logos\/bigEast\/XavierLogo.png",
     },
 }
 
