5f7c0536d9788540b7d2061357f8d8cc2dad5d96
NLeebs <lieberman.nathan@gmail.com>
2023-08-14 13:57:25 -0400

    Added conference data through Ivy league

--- src/functions/data-scraping/sendTeamStatsToFirebase.js
+++ src/functions/data-scraping/sendTeamStatsToFirebase.js
@@ -14,6 +14,10 @@ import bigSkyStatsURLs from "@/src/components/Add-To-Firebase/Team-Stats-URL-Obj
 import bigSouthStatsURLs from "@/src/components/Add-To-Firebase/Team-Stats-URL-Objects/bigSouth";
 import bigTenStatsURLs from "@/src/components/Add-To-Firebase/Team-Stats-URL-Objects/bigTen";
 import bigWestStatsURLs from "@/src/components/Add-To-Firebase/Team-Stats-URL-Objects/bigWest";
+import caaStatsURLs from "@/src/components/Add-To-Firebase/Team-Stats-URL-Objects/caa";
+import cusaStatsURLs from "@/src/components/Add-To-Firebase/Team-Stats-URL-Objects/cusa";
+import horizonStatsURLs from "@/src/components/Add-To-Firebase/Team-Stats-URL-Objects/horizon";
+import ivyStatsURLs from "@/src/components/Add-To-Firebase/Team-Stats-URL-Objects/ivy";
 
 ////// Team Stats and Color handler //////
 const pythonDataScrapeHandler = async (e) => {
@@ -23,10 +27,11 @@ const pythonDataScrapeHandler = async (e) => {
     const confURLsArr = [
         ['acc', accStatsURLs], ['americanAthletic', americaAthleticStatURLs], ['americanEast', americaEastStatURLs], ['asun', asunStatURLs], 
         ['atlantic10', atlantic10StatURLs], ['big12', big12StatsURLs], ['bigEast', bigEastStatsURLs], ['bigSky', bigSkyStatsURLs], ['bigSouth', bigSouthStatsURLs], 
-        ['bigTen', bigTenStatsURLs], ['bigWest', bigWestStatsURLs],
+        ['bigTen', bigTenStatsURLs], ['bigWest', bigWestStatsURLs], ['caa', caaStatsURLs], ['cusa', cusaStatsURLs], ['horizon', horizonStatsURLs], 
+        ['ivy', ivyStatsURLs],
     ];
 
-    const testURLArr=[['bigWest', bigWestStatsURLs]];
+    const testURLArr=[['ivy', ivyStatsURLs]];
 
     // Create team stats return object
     const teamStatsObj = {};
