70fd04fa9c0c52e284f1108a5c2a9445954c9d4f
NLeebs <lieberman.nathan@gmail.com>
2023-08-15 15:40:26 -0400

    Finished adding conference data to db

--- src/components/Add-To-Firebase/Team-Stats-URL-Objects/wac.js
+++ src/components/Add-To-Firebase/Team-Stats-URL-Objects/wac.js
@@ -1,66 +1,70 @@
 const wacStatURLs = {
     "Abilene Christian": {
-        "team-colors-URL": "",
-        "team-stats-URL": "",
-        "team-logo-URL": "team-logos\\wac\\Logo.png",
+        "team-colors-URL": "https://teamcolorcodes.com/abilene-christian-wildcats-colors/",
+        "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/abilene-christian-wildcats/stats",
+        "team-logo-URL": "team-logos\\wac\\AbileneChristianLogo.png",
         "list-name": "Abl Christian"
     },
     "California Baptist": {
-        "team-colors-URL": "",
-        "team-stats-URL": "",
-        "team-logo-URL": "team-logos\\wac\\Logo.png",
+        "team-colors-URL": "https://teamcolorcodes.com/california-baptist-lancers-colors/",
+        "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/california-baptist-lancers/stats",
+        "team-logo-URL": "team-logos\\wac\\CaliforniaBaptistLogo.png",
         "list-name": "Cal Baptist"
     },
     "Grand Canyon": {
-        "team-colors-URL": "",
-        "team-stats-URL": "",
-        "team-logo-URL": "team-logos\\wac\\Logo.png",
+        "team-colors-URL": "https://teamcolorcodes.com/gcu-antelopes-color-codes/",
+        "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/grand-canyon-antelopes/stats",
+        "team-logo-URL": "team-logos\\wac\\GrandCanyonLogo.png",
         "list-name": "Grd Canyon"
     },
     "Seattle": {
-        "team-colors-URL": "",
-        "team-stats-URL": "",
-        "team-logo-URL": "team-logos\\wac\\Logo.png",
+        "team-colors-URL": "https://teamcolorcodes.com/seattle-university-redhawks-color-codes/",
+        "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/seattle-redhawks/stats",
+        "team-logo-URL": "team-logos\\wac\\SeattleLogo.png",
     },
     "Southern Utah": {
-        "team-colors-URL": "",
-        "team-stats-URL": "",
-        "team-logo-URL": "team-logos\\wac\\Logo.png",
+        "team-colors-URL": "https://teamcolorcodes.com/southern-utah-thunderbirds-colors/",
+        "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/southern-utah-thunderbirds/stats",
+        "team-logo-URL": "team-logos\\wac\\SouthernUtahLogo.png",
         "list-name": "S Utah"
     },
     "Stephen F. Austin": {
-        "team-colors-URL": "",
-        "team-stats-URL": "",
-        "team-logo-URL": "team-logos\\wac\\Logo.png",
+        "team-colors-URL": "https://teamcolorcodes.com/stephen-f-austin-lumberjacks-colors/",
+        "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/stephen-f-austin-lumberjacks/stats",
+        "team-logo-URL": "team-logos\\wac\\StephenFAustinLogo.png",
         "list-name": "Ste F Austin",
         "mascot-name": "Lumberjacks",
     },
     "Tarleton State": {
         "team-colors-URL": "",
-        "team-stats-URL": "",
-        "team-logo-URL": "team-logos\\wac\\Logo.png",
+        "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/tarleton-state-texans/stats",
+        "team-logo-URL": "team-logos\\wac\\TarletonStLogo.png",
+        "mascot-name": "Texans",
+        "primary-color": "#4F2D7F",
+        "secondary-color": "#FFFFFF",
     },
     "Texas Rio Grande": {
-        "team-colors-URL": "",
-        "team-stats-URL": "",
-        "team-logo-URL": "team-logos\\wac\\Logo.png",
+        "team-colors-URL": "https://teamcolorcodes.com/utrgv-vaqueros-colors/",
+        "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/texas-rio-grande-valley-vaqueros/stats",
+        "team-logo-URL": "team-logos\\wac\\TexasRioGrandeLogo.png",
         "list-name": "TX-Pan Am"
     },
     "Texas-Arlington": {
-        "team-colors-URL": "",
-        "team-stats-URL": "",
-        "team-logo-URL": "team-logos\\wac\\Logo.png",
+        "team-colors-URL": "https://teamcolorcodes.com/texas-arlington-mavericks-color-codes/",
+        "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/texas-arlington-mavericks/stats",
+        "team-logo-URL": "team-logos\\wac\\TexasArlingtonLogo.png",
         "list-name": "TX-Arlington"
     },
     "Utah Tech": {
-        "team-colors-URL": "",
-        "team-stats-URL": "",
-        "team-logo-URL": "team-logos\\wac\\Logo.png",
+        "team-colors-URL": "https://teamcolorcodes.com/dixie-state-trailblazers-color-codes/",
+        "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/utah-tech-trailblazers/stats",
+        "team-logo-URL": "team-logos\\wac\\UtahTechLogo.png",
+        "mascot-name": "Trailblazers",
     },
     "Utah Valley": {
-        "team-colors-URL": "",
-        "team-stats-URL": "",
-        "team-logo-URL": "team-logos\\wac\\Logo.png",
+        "team-colors-URL": "https://teamcolorcodes.com/utah-valley-wolverines-colors/",
+        "team-stats-URL": "https://www.teamrankings.com/ncaa-basketball/team/utah-valley-wolverines/stats",
+        "team-logo-URL": "team-logos\\wac\\UtahValleyLogo.png",
         "list-name": "Utah Val St"
     },
 }
--- src/functions/data-scraping/sendTeamStatsToFirebase.js
+++ src/functions/data-scraping/sendTeamStatsToFirebase.js
@@ -24,7 +24,17 @@ import meacStatsURLs from "@/src/components/Add-To-Firebase/Team-Stats-URL-Objec
 import missouriValleyStatsURLs from "@/src/components/Add-To-Firebase/Team-Stats-URL-Objects/missouriValley";
 import mountainWestStatsURLs from "@/src/components/Add-To-Firebase/Team-Stats-URL-Objects/mountainWest";
 import necStatsURLs from "@/src/components/Add-To-Firebase/Team-Stats-URL-Objects/nec";
-import ohioValleyStatsURLs from "@/src/components/Add-To-Firebase/Team-Stats-URL-Objects/ohioValley";
+import ohioValleyStatsURLs from "@/src/components/Add-To-Firebase/Team-Stats-URL-Objects/ohioValley"; 
+import pac12StatsURLs from "@/src/components/Add-To-Firebase/Team-Stats-URL-Objects/pac12"; 
+import patriotStatsURLs from "@/src/components/Add-To-Firebase/Team-Stats-URL-Objects/patriot"; 
+import secStatsURLs from "@/src/components/Add-To-Firebase/Team-Stats-URL-Objects/sec"; 
+import southernStatsURLs from "@/src/components/Add-To-Firebase/Team-Stats-URL-Objects/southern"; 
+import southlandStatsURLs from "@/src/components/Add-To-Firebase/Team-Stats-URL-Objects/southland"; 
+import summitStatsURLs from "@/src/components/Add-To-Firebase/Team-Stats-URL-Objects/summit"; 
+import sunBeltStatsURLs from "@/src/components/Add-To-Firebase/Team-Stats-URL-Objects/sunBelt"; 
+import swacStatsURLs from "@/src/components/Add-To-Firebase/Team-Stats-URL-Objects/swac"; 
+import wacStatsURLs from "@/src/components/Add-To-Firebase/Team-Stats-URL-Objects/wac"; 
+import wccStatsURLs from "@/src/components/Add-To-Firebase/Team-Stats-URL-Objects/wcc"; 
 
 ////// Team Stats and Color handler //////
 const pythonDataScrapeHandler = async (e) => {
@@ -36,19 +46,22 @@ const pythonDataScrapeHandler = async (e) => {
         ['atlantic10', atlantic10StatURLs], ['big12', big12StatsURLs], ['bigEast', bigEastStatsURLs], ['bigSky', bigSkyStatsURLs], ['bigSouth', bigSouthStatsURLs], 
         ['bigTen', bigTenStatsURLs], ['bigWest', bigWestStatsURLs], ['caa', caaStatsURLs], ['cusa', cusaStatsURLs], ['horizon', horizonStatsURLs], 
         ['ivy', ivyStatsURLs], ['maac', maacStatsURLs], ['mac', macStatsURLs],  ['meac', meacStatsURLs], ['missouriValley', missouriValleyStatsURLs],
-        ['mountainWest', mountainWestStatsURLs], ['nec', necStatsURLs], ['ohioValley', ohioValleyStatsURLs],
+        ['mountainWest', mountainWestStatsURLs], ['nec', necStatsURLs], ['ohioValley', ohioValleyStatsURLs], ['pac12', pac12StatsURLs], ['patriot', patriotStatsURLs],
+        ['sec', secStatsURLs], ['southern', southernStatsURLs], ['southland', southlandStatsURLs], ['summit', summitStatsURLs], ['sunBelt', sunBeltStatsURLs], 
+        ['swac', swacStatsURLs], ['wac', wacStatsURLs], ['wcc', wccStatsURLs],
     ];
 
-    const testURLArr=[['ohioValley', ohioValleyStatsURLs]];
+    // To test individual conferences for errors
+    const testURLArr=[['wcc', wccStatsURLs]];
 
     // Create team stats return object
     const teamStatsObj = {};
-    testURLArr.forEach(teamArr => {
+    confURLsArr.forEach(teamArr => {
         teamStatsObj[teamArr[0]] = {};
     });
 
     // Call Python Script for each Conference and populate team stats object
-    await Promise.all(testURLArr.map( async confData => {
+    await Promise.all(confURLsArr.map( async confData => {
         const confRes = await fetch("http://127.0.0.1:5000/ncaa-stats", {
             method: "POST",
             headers: {
