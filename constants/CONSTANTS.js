335d3abf54aa848b44c13a4e886de6edeb69ac43
NLeebs <lieberman.nathan@gmail.com>
2023-09-06 23:21:19 -0400

    Added fouls that are shooting fouls constant

--- constants/CONSTANTS.js
+++ constants/CONSTANTS.js
@@ -5,3 +5,6 @@ export const NUMBER_OF_TEAMS = 360;
 export const AMOUNT_SEASON_GAMES = 30;
 export const AMOUNT_NONCONFERENCE_GAMES = 15;
 export const AMOUNT_CONFERENCE_GAMES = 15;
+
+// Game Stats
+export const PERCENT_FOULS_THAT_SHOOT_FT = .09;
