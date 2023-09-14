70546fc79ba2ad42ad192baa5097d66c1d05b7c2
NLeebs <lieberman.nathan@gmail.com>
2023-09-13 23:11:21 -0400

    Added highlight to teams that are going dancing

--- src/components/UI/TeamBar.js
+++ src/components/UI/TeamBar.js
@@ -3,15 +3,20 @@
 import React from "react";
 // React Functions
 import { useSelector } from "react-redux";
+// CSS Styles
+import styles from './TeamBar.module.css';
 
 
 // Component Function
 function TeamBar(props) {
   const regularSeasonRecords = useSelector((state) => state.regularSeasonRecords.records);
+  const tournamentTeamsArr = useSelector((state) => state.tournament.tournamentTeams);
 
   return (
     <div>
-        <div>{props.team} {regularSeasonRecords[props.team].wins}-{regularSeasonRecords[props.team].losses}</div>
+        <div className={
+          tournamentTeamsArr && tournamentTeamsArr.includes(props.team) && styles.goingToTheDance
+          }>{props.team} {regularSeasonRecords[props.team].wins}-{regularSeasonRecords[props.team].losses}</div>
     </div>
   );
 }
