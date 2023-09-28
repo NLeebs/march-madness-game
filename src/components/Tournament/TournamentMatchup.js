b50f48dd2c4bbb6b377a3bbe7c7ca8aa2710c151
NLeebs <lieberman.nathan@gmail.com>
2023-09-28 11:31:15 -0400

    Added tournement choice functionality

--- src/components/Tournament/TournamentMatchup.js
+++ src/components/Tournament/TournamentMatchup.js
@@ -1,17 +1,50 @@
 "use client"
 // Libraries
-import React from "react"
+import React, { useCallback } from "react"
 // React Functions
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
+// State
+import { tounramentPlayersPicksActions } from "@/store/tournamentPlayersPicksSlice";
 // Components
 import TeamBar from "../UI/TeamBar";
 
 
 function TournamentMatchup(props) {
-  
+    const dispatch = useDispatch();
+
+    const appState = useSelector((state) => state.appState)
+
+    // Account for change of regions in final four and beyond
+    let nextRoundRegion;
+    let teamIndex;
+    if (props.round === "elite eight" && props.region === "west") {nextRoundRegion = "eastWest"; teamIndex = 0}
+    else if (props.round === "elite eight" && props.region === "east") {nextRoundRegion = "eastWest"; teamIndex = 1}
+    else if (props.round === "elite eight" && props.region === "south") {nextRoundRegion = "southMidwest"; teamIndex = 0;}
+    else if (props.round === "elite eight" && props.region === "midwest") {nextRoundRegion = "southMidwest"; teamIndex = 1}
+    else if (props.round === "final four" && props.region === "eastWest") {nextRoundRegion = "championship"; teamIndex = 0}
+    else if (props.round === "final four" && props.region === "southMidwest") {nextRoundRegion = "championship"; teamIndex = 1}
+    else if (props.round === "finals") {nextRoundRegion = "champion"; teamIndex = 0;}
+    else {nextRoundRegion = props.region; teamIndex = props.index}
+
+    const teamSelectionClickHandler = useCallback((e) => {
+        const teamEl = e.target.closest('div.team-selection');
+        let opponentEl;
+        if (teamEl.getAttribute('value') === "0" ) opponentEl = teamEl.nextElementSibling;
+        else if (teamEl.getAttribute('value') === "1") opponentEl = teamEl.previousElementSibling;
+
+        dispatch(tounramentPlayersPicksActions.setPick({
+            round: props.round,
+            region: nextRoundRegion,
+            roundIndex: teamIndex,
+            team: teamEl.getAttribute('team'),
+            seed: teamEl.getAttribute('seed'),
+            opponent: opponentEl.getAttribute('team'),
+        }));
+    }, [dispatch, nextRoundRegion, props.round, teamIndex]);
+
     const matchupElements = props.matchup.map((teamObj, i) => {
         return (
-        <div key={i} className="flex items-center px-4 h-14 border-2 border-slate-100 rounded-md">
+        <div key={i} onClick={appState.tournament && teamSelectionClickHandler} value={i} team={teamObj.team} seed={teamObj.seed} className="team-selection flex items-center px-4 h-14 border-2 border-slate-100 rounded-md cursor-pointer">
             <div className="flex justify-center items-center w-6 pr-2">
                 {teamObj.seed}
             </div>
--- store/tournamentPlayersPicksSlice.js
+++ store/tournamentPlayersPicksSlice.js
@@ -21,12 +21,15 @@ const initalState = {
         midwest: [[{team: "", seed: ""}, {team: "", seed: ""}]],
     },
     roundFinalFourPicks: {
-        eastSouth: [[{team: "", seed: ""}, {team: "", seed: ""}]],
-        westMidwest: [[{team: "", seed: ""}, {team: "", seed: ""}]],
+        eastWest: [[{team: "", seed: ""}, {team: "", seed: ""}]],
+        southMidwest: [[{team: "", seed: ""}, {team: "", seed: ""}]],
     },
     roundFinalsPicks: {
         championship: [[{team: "", seed: ""}, {team: "", seed: ""}]],
     },
+    champion: {
+        champion: [[{team: "", seed: ""}]],
+    }
 };
 
 const tournamentPlayersPicksSlice = createSlice({
@@ -34,7 +37,36 @@ const tournamentPlayersPicksSlice = createSlice({
     initialState: initalState,
     reducers: {
         setPick(state, action) {
+            let whichRound;
+            let removeRoundPicksArr;
+            if (action.payload.round === "1") {whichRound = "roundTwoPicks"; removeRoundPicksArr = ["roundSweetSixteenPicks", "roundEliteEightPicks", "roundFinalFourPicks", "roundFinalsPicks", "champion"];}
+            if (action.payload.round === "2") {whichRound = "roundSweetSixteenPicks"; removeRoundPicksArr = ["roundEliteEightPicks", "roundFinalFourPicks", "roundFinalsPicks", "champion"];}
+            if (action.payload.round === "sweet sixteen") {whichRound = "roundEliteEightPicks"; removeRoundPicksArr = ["roundFinalFourPicks", "roundFinalsPicks", "champion"];}
+            if (action.payload.round === "elite eight") {whichRound = "roundFinalFourPicks"; removeRoundPicksArr = ["roundFinalsPicks", "champion"];}
+            if (action.payload.round === "final four") {whichRound = "roundFinalsPicks"; removeRoundPicksArr = ["champion"];}
+            if (action.payload.round === "finals") {whichRound = "champion"; removeRoundPicksArr = [];}
 
+            // Set Pick
+            if (action.payload.team !== "") {
+                state[whichRound][action.payload.region][Math.floor(+action.payload.roundIndex / 2)][+action.payload.roundIndex % 2].team = action.payload.team;
+                state[whichRound][action.payload.region][Math.floor(+action.payload.roundIndex / 2)][+action.payload.roundIndex % 2].seed = action.payload.seed;
+            }
+
+            // Remove erroneous future picks
+            if (action.payload.opponent !== "") {
+                removeRoundPicksArr.forEach((round) => {
+                    Object.keys(state[round]).forEach((region) => {
+                        state[round][region].forEach((matchup) => {
+                            matchup.forEach((teamObj) => {
+                                if (teamObj.team === action.payload.opponent) {
+                                    teamObj.team = "";
+                                    teamObj.seed = "";
+                                }
+                            });
+                        });
+                    });
+                });
+            }
         },
     },
 });
