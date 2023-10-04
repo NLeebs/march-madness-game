b4e6a27ab9ca047156dc02d71702ce70202b3422
NLeebs <lieberman.nathan@gmail.com>
2023-10-04 15:08:43 -0400

    Fixed tournament order to reflect top teams and playin games correctly

--- src/components/Tournament/Tournament.js
+++ src/components/Tournament/Tournament.js
@@ -1,6 +1,6 @@
 "use client"
 // Libraries
-import React  from "react"
+import React, { Fragment }  from "react"
 // React Functions
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux"
@@ -9,6 +9,7 @@ import { tournamentActions } from "@/store/tournamentSlice";
 // Components
 import TournamentRound from "./TournamentRound";
 import PlayTournamentButton from "../UI/PlayTournamentButton";
+import PlayPlayinGames from "./PlayPlayinGames";
 
 function Tournament(props) {
     const dispatch = useDispatch();
@@ -18,60 +19,62 @@ function Tournament(props) {
     }, [dispatch])
 
     return (
-        <div className="grid grid-flow-row">
-            <div className="grid grid-flow-col">
-                <div className="col-span-1 flex flex-col gap-y-16 min-w-300">
-                    <TournamentRound region="west" round="1" />
-                    <TournamentRound region="east" round="1" />
-                </div>
-                <div className="col-span-1 flex flex-col gap-y-16 min-w-300">
-                    <TournamentRound region="west" round="2" />
-                    <TournamentRound region="east" round="2" />
-                </div>
-                <div className="col-span-1 flex flex-col gap-y-16 min-w-300">
-                    <TournamentRound region="west" round="sweet sixteen" />
-                    <TournamentRound region="east" round="sweet sixteen" />
-                </div>
-                <div className="col-span-1 flex flex-col gap-y-16 min-w-300">
-                    <TournamentRound region="west" round="elite eight" />
-                    <TournamentRound region="east" round="elite eight" />
-                </div>
-                <div className="col-span-1 flex flex-col gap-y-16 min-w-300">
-                    <TournamentRound region="eastWest" round="final four" />
-                </div>
-                <div className="col-span-1 flex flex-col gap-y-16">
-                    <div className="flex flex-col gap-y-16 min-w-300">
-                        <TournamentRound region="championship" round="finals" />
+            <Fragment>
+                <div className="grid grid-flow-row">
+                    <div className="grid grid-flow-col">
+                        <div className="col-span-1 flex flex-col gap-y-16 min-w-300">
+                            <TournamentRound region="east" round="1" />
+                            <TournamentRound region="west" round="1" />
+                        </div>
+                        <div className="col-span-1 flex flex-col gap-y-16 min-w-300">
+                            <TournamentRound region="east" round="2" />
+                            <TournamentRound region="west" round="2" />
+                        </div>
+                        <div className="col-span-1 flex flex-col gap-y-16 min-w-300">
+                            <TournamentRound region="east" round="sweet sixteen" />
+                            <TournamentRound region="west" round="sweet sixteen" />
+                        </div>
+                        <div className="col-span-1 flex flex-col gap-y-16 min-w-300">
+                            <TournamentRound region="east" round="elite eight" />
+                            <TournamentRound region="west" round="elite eight" />
+                        </div>
+                        <div className="col-span-1 flex flex-col gap-y-16 min-w-300">
+                            <TournamentRound region="eastWest" round="final four" />
+                        </div>
+                        <div className="col-span-1 flex flex-col gap-y-16">
+                            <div className="flex flex-col gap-y-16 min-w-300">
+                                <TournamentRound region="championship" round="finals" />
+                            </div>
+                            <div className="flex flex-col gap-y-16 min-w-300">
+                                <TournamentRound region="champion" round="champion" />
+                            </div>
+                            <PlayTournamentButton />
+                        </div>
+                        <div className="col-span-1 flex flex-col gap-y-16 min-w-300">
+                            <TournamentRound region="southMidwest" round="final four" />
+                        </div>
+                        <div className="col-span-1 flex flex-col gap-y-16 min-w-300">
+                            <TournamentRound region="south" round="elite eight" />
+                            <TournamentRound region="midwest" round="elite eight" />
+                        </div>
+                        <div className="col-span-1 flex flex-col gap-y-16 min-w-300">
+                            <TournamentRound region="south" round="sweet sixteen" />
+                            <TournamentRound region="midwest" round="sweet sixteen" />
+                        </div>
+                        <div className="col-span-1 flex flex-col gap-y-16 min-w-300">
+                            <TournamentRound region="south" round="2" />
+                            <TournamentRound region="midwest" round="2" />
+                        </div>
+                        <div className="col-span-1 flex flex-col gap-y-16 min-w-300">
+                            <TournamentRound region="south" round="1" />
+                            <TournamentRound region="midwest" round="1" />
+                        </div>
                     </div>
-                    <div className="flex flex-col gap-y-16 min-w-300">
-                        <TournamentRound region="champion" round="champion" />
+                    <div>
+                        <TournamentRound region="playin" round="playin" />
                     </div>
-                    <PlayTournamentButton />
                 </div>
-                <div className="col-span-1 flex flex-col gap-y-16 min-w-300">
-                    <TournamentRound region="southMidwest" round="final four" />
-                </div>
-                <div className="col-span-1 flex flex-col gap-y-16 min-w-300">
-                    <TournamentRound region="south" round="elite eight" />
-                    <TournamentRound region="midwest" round="elite eight" />
-                </div>
-                <div className="col-span-1 flex flex-col gap-y-16 min-w-300">
-                    <TournamentRound region="south" round="sweet sixteen" />
-                    <TournamentRound region="midwest" round="sweet sixteen" />
-                </div>
-                <div className="col-span-1 flex flex-col gap-y-16 min-w-300">
-                    <TournamentRound region="south" round="2" />
-                    <TournamentRound region="midwest" round="2" />
-                </div>
-                <div className="col-span-1 flex flex-col gap-y-16 min-w-300">
-                    <TournamentRound region="south" round="1" />
-                    <TournamentRound region="midwest" round="1" />
-                </div>
-            </div>
-            <div>
-                <TournamentRound region="playin" round="playin" />
-            </div>
-        </div>
+        </Fragment>
     );
 }
 
