7ffa8b83f97873c54c7fd6583785a11bc3b5a3f8
NLeebs <lieberman.nathan@gmail.com>
2023-10-10 15:49:08 -0400

    Split repeatable setup of tournament result obj to new function

---
+++ src/functions/tournament/getTournamentGameResultObj.js
@@ -0,0 +1,26 @@
+export default function getTournamentGameResultObj(resultObj, gameIndex, region = null) {        
+
+    // Construct Results Object
+    let winningTeam;
+    let winningScore;
+    let losingScore;
+
+    if (resultObj.favoredScore > resultObj.underdogScore) {
+        winningTeam = resultObj.favoredTeam; 
+        winningScore = resultObj.favoredScore;
+        losingScore = resultObj.underdogScore;
+    }
+    else {
+        winningTeam = resultObj.underdogTeam;
+        winningScore = resultObj.underdogScore;
+        losingScore = resultObj.favoredScore;
+    }
+
+    return {
+        region,
+        gameIndex,
+        winningTeam,
+        winningScore,
+        losingScore
+    };
+}
