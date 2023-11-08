3f9874bebb4cc1bfc5a55969e9e6ca3638cafd2b
NLeebs <lieberman.nathan@gmail.com>
2023-11-08 15:58:33 -0500

    It has a title and title font!

--- src/components/UI/StartScreen.js
+++ src/components/UI/StartScreen.js
@@ -8,7 +8,9 @@ import StartButton from "./StartButton";
 function StartScreen() {
  
   return (
-  <div className="w-screen h-screen flex justify-center items-center">
+  <div className="w-screen h-screen flex flex-col justify-center items-center">
+    <h1>Madness</h1>
+    <h2 className="pb-4">The Game</h2>
     <StartButton />
   </div>
   );
