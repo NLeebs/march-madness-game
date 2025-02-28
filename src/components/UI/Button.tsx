cffd7cf58eefeb324a5bb6ed8635649fb2e2360b
NLeebs <lieberman.nathan@gmail.com>
2025-02-28 11:15:25 -0500

    Added types to button and exported

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -1,24 +1,27 @@
-"use client"
-// Libraries
+"use client";
 import React from "react";
 
+interface ButtonProps {
+  onClick: () => void;
+  backgroundColor: string;
+  text: string;
+}
 
-// Component Function
-function Button(props) {
-  
-
+export const Button: React.FC<ButtonProps> = ({
+  onClick,
+  backgroundColor,
+  text,
+}) => {
   return (
     <button
       type="button"
       className="rounded-full px-6 py-4 text-lg font-semibold text-neutral-50 shadow-sm transition-transform ease-out hover:scale-110 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-neutral-300"
-      onClick={props.onClick}
+      onClick={onClick}
       style={{
-        backgroundColor: props.backgroundColor,
+        backgroundColor: backgroundColor,
       }}
     >
-      {props.text}
+      {text}
     </button>
-    );
-}
-
-export default Button;
+  );
+};
