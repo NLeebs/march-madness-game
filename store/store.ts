6c3b807dbd0922fc4a905805e6f4acfe5801207a
NLeebs <lieberman.nathan@gmail.com>
2025-03-06 11:33:23 -0500

    Created RootState type in store and exported state actions in store folder

--- store/store.ts
+++ store/store.ts
@@ -1,5 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-
 import teamStatsSlice from "./teamStatsSlice";
 import teamScheduleSlice from "./teamScheduleSlice";
 import appStateSlice from "./appStateSlice";
@@ -8,17 +7,17 @@ import regularSeasonRecordSlice from "./regularSeasonRecordSlice";
 import tournamentSlice from "./tournamentSlice";
 import tournamentPlayersPicksSlice from "./tournamentPlayersPicksSlice";
 
-
 const store = configureStore({
-    reducer: {
-        appState: appStateSlice.reducer,
-        uiState: uiStateSlice.reducer,
-        teamStats: teamStatsSlice.reducer,
-        teamSchedule: teamScheduleSlice.reducer,
-        regularSeasonRecords: regularSeasonRecordSlice.reducer,
-        tournament: tournamentSlice.reducer,
-        tournamentPlayersPicks: tournamentPlayersPicksSlice.reducer,
-    }
+  reducer: {
+    appState: appStateSlice.reducer,
+    uiState: uiStateSlice.reducer,
+    teamStats: teamStatsSlice.reducer,
+    teamSchedule: teamScheduleSlice.reducer,
+    regularSeasonRecords: regularSeasonRecordSlice.reducer,
+    tournament: tournamentSlice.reducer,
+    tournamentPlayersPicks: tournamentPlayersPicksSlice.reducer,
+  },
 });
 
+export type RootState = ReturnType<typeof store.getState>;
 export default store;
