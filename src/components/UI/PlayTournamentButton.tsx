fb88ec7217c5034d7a753655b326f029fd5ace1f
NLeebs <lieberman.nathan@gmail.com>
2025-06-10 16:23:11 -0400

    Converted PlayTournamentButton to typescript

--- src/components/UI/PlayTournamentButton.tsx
+++ src/components/UI/PlayTournamentButton.tsx
@@ -2,15 +2,14 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState, appStateActions } from "@/store";
-import { Tournament } from "@/types";
 import { BasketballSVG } from "@/src/components/Graphics";
 import { PRIMARY_COLOR, SECONDARY_COLOR } from "@/src/constants";
 
 export const PlayTournamentButton = () => {
   const dispatch = useDispatch();
 
-  const playerPicksObj = useSelector<RootState, Tournament>(
-    (state) => state.tournamentPlayersPicks.picks
+  const playerPicksObj = useSelector(
+    (state: RootState) => state.tournamentPlayersPicks.picks
   );
 
   const activateTournamentPlay = () => {
@@ -28,35 +27,39 @@ export const PlayTournamentButton = () => {
   });
 
   return (
-    <button
-      disabled={!isAllPicksSelected}
-      onClick={activateTournamentPlay}
-      className={`relative rounded-full transition-transform ease-out "hover:scale-110" focus-visible:outline-neutral-300`}
-    >
-      <div
-        className={`absolute inset-0 w-full h-full z-10 flex justify-center items-center rounded-full ${
-          !isAllPicksSelected ? "opacity-50" : "opacity-20"
-        }`}
-        style={{
-          backgroundColor: `${!isAllPicksSelected ? "#d1d5db" : PRIMARY_COLOR}`,
-        }}
-      ></div>
-      <div
-        className={`absolute inset-0 w-full h-full z-10 flex justify-center items-center rounded-full`}
+    <div className="flex justify-center items-center">
+      <button
+        disabled={!isAllPicksSelected}
+        onClick={activateTournamentPlay}
+        className={`relative w-[14rem] rounded-full transition-transform ease-out "hover:scale-110" focus-visible:outline-neutral-300`}
       >
-        <h3 className={`${"text-neutral-50"}`}>
-          {!isAllPicksSelected ? "Fill In Bracket" : "Submit Picks"}
-        </h3>
-      </div>
-      <div
-        className={`${isAllPicksSelected && "motion-safe:animate-spin-slow"}`}
-      >
-        <BasketballSVG
-          size={225}
-          basketballColor={PRIMARY_COLOR}
-          seamColor={SECONDARY_COLOR}
-        />
-      </div>
-    </button>
+        <div
+          className={`absolute inset-0 w-full h-full z-10 flex justify-center items-center rounded-full ${
+            !isAllPicksSelected ? "opacity-50" : "opacity-20"
+          }`}
+          style={{
+            backgroundColor: `${
+              !isAllPicksSelected ? "#d1d5db" : PRIMARY_COLOR
+            }`,
+          }}
+        ></div>
+        <div
+          className={`absolute inset-0 w-full h-full z-10 flex justify-center items-center rounded-full`}
+        >
+          <h3 className={"text-neutral-50"}>
+            {!isAllPicksSelected ? "Fill In Bracket" : "Submit Picks"}
+          </h3>
+        </div>
+        <div
+          className={`${isAllPicksSelected && "motion-safe:animate-spin-slow"}`}
+        >
+          <BasketballSVG
+            size={224}
+            basketballColor={PRIMARY_COLOR}
+            seamColor={SECONDARY_COLOR}
+          />
+        </div>
+      </button>
+    </div>
   );
 };
