5333f7ae093565c8bfd61b065a1bb10a1d80ef25
NLeebs <lieberman.nathan@gmail.com>
2025-06-24 11:59:00 -0400

    Convert BracketRound to typescript

--- src/components/Bracket/BracketRound.tsx
+++ src/components/Bracket/BracketRound.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 
-export const BracketRound = ({ teams }) => {
+interface BracketRoundProps {
+  teamNames: string[];
+}
+
+export const BracketRound: React.FC<BracketRoundProps> = ({ teamNames }) => {
   return (
     <div className="flex flex-col space-y-2">
-      {teams.map((team, index) => (
+      {teamNames.map((teamName, index) => (
         <div
           key={index}
           className={`w-24 h-10 bg-white border-2 rounded-md flex justify-center items-center ${
             index % 2 === 0 ? "border-blue-600" : "border-red-600"
           }`}
         >
-          <p className="text-sm">{team}</p>
+          <p className="text-sm">{teamName}</p>
         </div>
       ))}
     </div>
