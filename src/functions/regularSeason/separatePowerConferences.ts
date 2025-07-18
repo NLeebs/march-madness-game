a085e4262d1996051e87e23fb7f72afc1e259b17
NLeebs <lieberman.nathan@gmail.com>
2025-07-18 14:44:20 -0400

    Convert separatePowerConferences to Typescript

---
+++ src/functions/regularSeason/separatePowerConferences.ts
@@ -0,0 +1,17 @@
+import { ConferenceMap } from "@/schemas";
+
+export const separatePowerConferences = (
+  teamStats: ConferenceMap,
+  powerConfArr: string[]
+) => {
+  const otherConfArr = [];
+
+  for (const key in teamStats) {
+    if (powerConfArr.includes(key)) {
+    } else {
+      otherConfArr.push(key);
+    }
+  }
+
+  return otherConfArr;
+};
