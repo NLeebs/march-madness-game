1c87252bb48620a6789a3889ef0d567be69de623
NLeebs <lieberman.nathan@gmail.com>
2026-02-25 11:56:33 -0500

    fix: line color dynamic style moved to inline style

--- src/components/UI/LineSpacer.tsx
+++ src/components/UI/LineSpacer.tsx
@@ -6,7 +6,10 @@ interface LineSpacerProps {
 }
 
 export const LineSpacer = ({ lineColor }: LineSpacerProps) => {
-  const borderColor = `border-[${lineColor}]`;
-
-  return <hr className={cn("w-full my-6 border-t opacity-50", borderColor)} />;
+  return (
+    <hr
+      style={{ borderColor: lineColor }}
+      className="w-full my-6 border-t opacity-50"
+    />
+  );
 };
